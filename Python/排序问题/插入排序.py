e29dc576ad6e9e071b8a361dca2d2cf29ae9cb21
yuki301 <61863085+yuki301@users.noreply.github.com>
2023-07-05 20:32:46 +0800

    Add files via upload

---
+++ Python/排序问题/插入排序.py
@@ -0,0 +1,13 @@
+def insertion_sort(A):
+    for j in range(2,len(A)):
+        key=A[j]
+        i=j-1
+        while i>0 and A[i]>key:#此处A[i]与key比较使用大于号，表明此排序为升序
+            A[i+1]=A[i]
+            i-=1
+        A[i+1]=key
+
+
+A=[0,532,-32135,930,12.4]
+insertion_sort(A)
+print(A)
